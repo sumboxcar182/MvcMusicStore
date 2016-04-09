982be50840b2e12dde50da3ef5d688d7be83770a
sumboxcar182 <atcsumboxcar182@hotmail.com>
2016-04-09 12:35:24 -0400

    Setting up an Album and Artist Class

---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
