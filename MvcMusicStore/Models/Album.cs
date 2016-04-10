417ad8be465e31240924eaf79f53e88c93a70fce
sumboxcar182 <atcsumboxcar182@hotmail.com>
2016-04-10 09:54:49 -0400

    Reverted changes and removed two staging files

--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -7,11 +7,5 @@ namespace MvcMusicStore.Models
 
         public int AlbumID { get; set; }
         public string Title { get; set; }
-        public Artist Artist { get; set; }
-
-        public int ReviewID { get; set; }
-
-        public virtual List<Review> Review { get; set; }
-        
     }
 }
